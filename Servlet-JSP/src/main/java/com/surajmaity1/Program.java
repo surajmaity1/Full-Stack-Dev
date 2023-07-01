1e76b2460771bacc433239461321a08689dd6bf8
surajmaity1 <surajmaity981@gmail.com>
2023-07-01 17:22:56 +0530

    feat: Servlet Project

---
+++ Servlet-JSP/src/main/java/com/surajmaity1/Program.java
@@ -0,0 +1,19 @@
+package com.surajmaity1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Program extends HttpServlet{
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		int val1 = Integer.parseInt(req.getParameter("num1"));
+		int val2 = Integer.parseInt(req.getParameter("num2"));
+		int total = val1 + val2;
+		
+		PrintWriter pw = res.getWriter();
+		pw.println("Result: " + total);
+	}
+}
