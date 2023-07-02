d5af54b9e6e96e9a94871f1ae11d7c2b3c2a06f2
surajmaity1 <surajmaity981@gmail.com>
2023-07-02 10:15:59 +0530

    feat: ServletConfig and ServletContext

--- Servlet-JSP/src/main/java/com/surajmaity1/Program.java
+++ Servlet-JSP/src/main/java/com/surajmaity1/Program.java
@@ -1,21 +1,43 @@
 package com.surajmaity1;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Program extends HttpServlet{
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+
+		//  <!-- If diff servlet have diff value, this task will be performed by ServletConfig -->
+		ServletConfig sc = getServletConfig();
+		String str = sc.getInitParameter("name");
+		
+		PrintWriter pw = res.getWriter();
+		pw.println("Hi: " + str);
+		
+		//<!-- If every servlet want to share same value, this task will be performed by ServletContext -->
+		/*ServletContext sc = getServletContext();
+		String str = sc.getInitParameter("name");
+		
+		PrintWriter pw = res.getWriter();
+		pw.println("Hi: " + str);
+		*/
+		
+		/*
+		
 		int val1 = Integer.parseInt(req.getParameter("num1"));
+		
 		int val2 = Integer.parseInt(req.getParameter("num2"));
 		int total = val1 + val2;
 		
-		// This technique called Session Management
-		// There are three concept to Transfer data from one Servlet to Another Servlet
+		 This technique called Session Management
+		 There are three concept to Transfer data from one Servlet to Another Servlet
+		 */
 		/*
 		 * 1. Session
 		 * 2. Cookie
@@ -24,9 +46,9 @@ public class Program extends HttpServlet{
 		
 		
 		// Here we have used Cookie technique	
-		Cookie c = new Cookie("total", total+"");
+		/*Cookie c = new Cookie("total", total+"");
 		res.addCookie(c);
-		res.sendRedirect("SquareNum");
+		res.sendRedirect("SquareNum");*/
 		
 		// Here we have used Session technique	
 		// HttpSession session = req.getSession();
@@ -45,6 +67,7 @@ public class Program extends HttpServlet{
 		
 		PrintWriter pw = res.getWriter();
 		pw.println("Result: " + total);
+		
 		*/
 	}
 }
