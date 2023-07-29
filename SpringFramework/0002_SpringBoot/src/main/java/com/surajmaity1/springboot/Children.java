39cc47bc6d54e86601886be2396304b001e2a662
surajmaity1 <surajmaity981@gmail.com>
2023-07-29 09:04:00 +0530

    feat: @Autowired in Spring Boot

---
+++ SpringFramework/0002_SpringBoot/src/main/java/com/surajmaity1/springboot/Children.java
@@ -0,0 +1,10 @@
+package com.surajmaity1.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Children {
+	public void callChildren() {
+		System.out.println("Children is called ...");
+	}
+}
