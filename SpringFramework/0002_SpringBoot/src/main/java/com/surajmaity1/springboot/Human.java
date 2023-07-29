8c69ad5bac75d9f3440a5d72ce87dfe4ac6c31f0
surajmaity1 <surajmaity981@gmail.com>
2023-07-29 08:57:26 +0530

    feat: Dependency Injection in Spring

---
+++ SpringFramework/0002_SpringBoot/src/main/java/com/surajmaity1/springboot/Human.java
@@ -0,0 +1,10 @@
+package com.surajmaity1.springboot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Human {
+	public void speak() {
+		System.out.println("Human can speak");
+	}
+}
