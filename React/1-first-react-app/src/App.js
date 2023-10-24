d1fb21b68ce6e46640fc3e7110f69ce2dd30b6d3
surajmaity1 <surajmaity981@gmail.com>
2023-10-24 08:17:53 +0530

    feat: file restructured [ include JS, React ]

---
+++ React/1-first-react-app/src/App.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function App() {
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+
+  useState(function () {
+    getAdvice();
+  }, []);
+
+  return (
+    <div>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
+    </div>
+  );
+}
+
+function Message(props) {
+  return (
+    <p>
+      <strong>{props.count}</strong> fetched
+    </p>
+  );
+}
