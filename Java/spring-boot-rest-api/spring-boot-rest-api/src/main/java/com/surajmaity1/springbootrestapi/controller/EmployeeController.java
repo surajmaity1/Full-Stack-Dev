a8172cbbc8b40c297c738b76c393137ad7dfdb52
Suraj Maity <surajmaity981@gmail.com>
2023-10-27 09:03:17 +0530

    feat: DevTools Dependency and Logger [slf4j]

--- Java/spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/surajmaity1/springbootrestapi/controller/EmployeeController.java
+++ Java/spring-boot-rest-api/spring-boot-rest-api/src/main/java/com/surajmaity1/springbootrestapi/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import com.surajmaity1.springbootrestapi.service.EmployeeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +20,8 @@ import java.util.List;
 public class EmployeeController {
 
     EmployeeService employeeService;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -56,6 +60,8 @@ public class EmployeeController {
     @PostMapping
     public String registerEmployee(@RequestBody Employee employee) {
         employeeService.registerEmployee(employee);
+        LOGGER.info("Employee Created");
+        LOGGER.debug("Employee Debug Logger enabled");
         return "SUCCESS: Employee Registered !!";
     }
 
